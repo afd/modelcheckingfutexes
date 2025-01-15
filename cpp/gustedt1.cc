f8b11a368a5c65b02915bf3bbcfdc5c2b3449180
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2025-01-15 14:38:55 +0000

    set_lock -> set_locked

--- cpp/gustedt1.cc
+++ cpp/gustedt1.cc
@@ -1,18 +1,18 @@
 #define BUSYWAIT 10
 
-#define set_lock(VAL)  (0x80000000u | (VAL))
+#define set_locked(VAL)  (0x80000000u | (VAL))
 #define is_locked(VAL) (0x80000000u & (VAL))
 class Mutex {
 public:
   Mutex() : futex_word(0) {}
   void lock() {
-    uint32_t cur = cmpxchg(futex_word, 0, set_lock(1)); /*@\label{line:gustedt1:firstcmpxchg}@*/
+    uint32_t cur = cmpxchg(futex_word, 0, set_locked(1)); /*@\label{line:gustedt1:firstcmpxchg}@*/
     if (cur == 0) return; /*@\label{line:gustedt1:fastpathreturn}@*/
     cur = futex_word.fetch_add(1) + 1; /*@\label{line:gustedt1:increment}@*/
     for (;;) { /*@\label{line:gustedt1:lockloop}@*/
       while (!is_locked(cur)) { /*@\label{line:gustedt1:subloop1}@*/
         uint32_t prev =
-          cmpxchg(futex_word, cur, set_lock(cur)); /*@\label{line:gustedt1:secondcmpxchg}@*/
+          cmpxchg(futex_word, cur, set_locked(cur)); /*@\label{line:gustedt1:secondcmpxchg}@*/
         if (prev == cur) return; /*@\label{line:gustedt1:return}@*/
         for (uint32_t i = 0;
              i < BUSYWAIT && is_locked(cur);
@@ -27,8 +27,8 @@ public:
     }
   }
   void unlock() {
-    uint32_t prev = futex_word.fetch_sub(set_lock(1)); /*@\label{line:gustedt1:decrement}@*/
-    if (prev != set_lock(1)) {
+    uint32_t prev = futex_word.fetch_sub(set_locked(1)); /*@\label{line:gustedt1:decrement}@*/
+    if (prev != set_locked(1)) {
       futex_wake(&futex_word, 1); /*@\label{line:gustedt1:futexwake}@*/
     }
   }
--- cpp/gustedt2.cc
+++ cpp/gustedt2.cc
@@ -1,18 +1,18 @@
 #define BUSYWAIT 10
 
-#define set_lock(VAL)   (0x80000000u | (VAL))
-#define unset_lock(VAL) (0x7FFFFFFFu & (VAL))
+#define set_locked(VAL)   (0x80000000u | (VAL))
+#define unset_locked(VAL) (0x7FFFFFFFu & (VAL))
 #define is_locked(VAL)  (0x80000000u & (VAL))
 class Mutex {
 public:
   Mutex() : futex_word(0) {}
   void lock() {
-    uint32_t cur = cmpxchg(futex_word, 0, set_lock(1)); /*@\label{line:gustedt2:firstcmpxchg}@*/
+    uint32_t cur = cmpxchg(futex_word, 0, set_locked(1)); /*@\label{line:gustedt2:firstcmpxchg}@*/
     if (cur == 0) return; /*@\label{line:gustedt2:fastpathreturn}@*/
     for (uint32_t i = 0; i < BUSYWAIT; i++) { /*@\label{line:gustedt2:busywait}@*/
-      if (is_locked(cur)) cur = unset_lock(cur) - 1; /*@\label{line:gustedt2:busywaitdec}@*/
+      if (is_locked(cur)) cur = unset_locked(cur) - 1; /*@\label{line:gustedt2:busywaitdec}@*/
       uint32_t prev =
-        cmpxchg(futex_word, cur, set_lock(cur+1)); /*@\label{line:gustedt2:busywaitcmpxchg}@*/
+        cmpxchg(futex_word, cur, set_locked(cur+1)); /*@\label{line:gustedt2:busywaitcmpxchg}@*/
       if (prev == cur) return;
       cur = prev;
     }
@@ -20,17 +20,17 @@ public:
     for (;;) { /*@\label{line:gustedt2:unboundedloop}@*/
       if (is_locked(cur)) {
         futex_wait(&futex_word, cur);
-        cur = unset_lock(cur) - 1;
+        cur = unset_locked(cur) - 1;
       }
       uint32_t prev =
-        cmpxchg(futex_word, cur, set_lock(cur)); /*@\label{line:gustedt2:slowpathcmpxchg}@*/
+        cmpxchg(futex_word, cur, set_locked(cur)); /*@\label{line:gustedt2:slowpathcmpxchg}@*/
       if (prev == cur) return; /*@\label{line:gustedt2:slowpathreturn}@*/
       cur = prev;
     }
   }
   void unlock() {
-    uint32_t prev = futex_word.fetch_sub(set_lock(1));
-    if (prev != set_lock(1)) {
+    uint32_t prev = futex_word.fetch_sub(set_locked(1));
+    if (prev != set_locked(1)) {
       futex_wake(&futex_word, 1);
     }
   }
